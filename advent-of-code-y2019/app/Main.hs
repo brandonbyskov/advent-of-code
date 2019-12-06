385b5c80a7f4d984a4e2c2a61a2dc33bf1a55ab3
brandonbyskov <bbyskov@cj.com>
2019-12-06 15:38:52 -0800

    Puzzle 2

--- advent-of-code-y2019/app/Main.hs
+++ advent-of-code-y2019/app/Main.hs
@@ -3,4 +3,5 @@ module Main where
 import AOC2019
 
 main :: IO ()
-main = do putStrLn =<< show <$> puzzle1
+main = do putStrLn =<< ("Puzzle 1: " <>) <$> show <$> puzzle1
+          putStrLn =<< ("Puzzle 2: " <>) <$> show <$> puzzle2
--- advent-of-code-y2019/src/AOC2019.hs
+++ advent-of-code-y2019/src/AOC2019.hs
@@ -1,6 +1,7 @@
 module AOC2019
-    ( puzzle1
-    ) where
+  ( puzzle1
+  , puzzle2
+  ) where
 
 puzzle1 :: IO Int
 puzzle1 = do inputs <- readFileLines "data/input1.txt"
@@ -12,6 +13,19 @@ puzzle1 = do inputs <- readFileLines "data/input1.txt"
     fuelNeeded a
       | a <= 8    = 0
       | otherwise = (a `div` 3) - 2
+
+puzzle2 :: IO Int
+puzzle2 = do inputs <- readFileLines "data/input1.txt"
+             return $ puzzle2' inputs
+  where
+    puzzle2' :: [Int] -> Int
+    puzzle2' xs = sum $ fmap fuelNeeded xs
+    fuelNeeded :: Int -> Int
+    fuelNeeded mass
+          | mass <= 8 = 0
+          | otherwise = let fuelMass = (mass `div` 3) - 2
+                        in fuelMass + fuelNeeded fuelMass
+
 
 readFileLines :: String -> IO [Int]
 readFileLines filename = fmap read <$> lines <$> readFile filename
